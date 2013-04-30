43e1672552738289353482a263be00fdc0f490d8
JStyleM <jhonatanmoncada@gmail.com>
2013-04-30 17:49:11 -0500

    Añadiendo Archivos
    Anadiendo los primeros archis solo el index

---
+++ default.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Account activated!</title>
+<style type="text/css">
+body {
+	background-color: #0D0D0D;
+	font-family:Arial, Helvetica, sans-serif; font-size:100%; 
+}
+body,td,th {
+	color: #FFF;
+}
+</style>
+</head>
+
+<body>
+<center>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><img src="http://www.2freehosting.com/images/logo.png" width="257" height="99" />
+</p>
+<p>&nbsp;</p>
+<p>Thank you for choosing <a href="http://www.2freehosting.com">free hosting</a> services with <a href="http://www.2freehosting.com" style="color:#FFF">2freehosting.com</a>, your account was successfully activated. <br />
+  Please upload your website. </p>
+</center>
+</body>
+</html>
+<!--DEFAULT_WELCOME_PAGE-->
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+ 	   	<meta charset="utf-8">
+ 	   	<meta name="author" content="JMB">
+ 	   	<script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>
+ 	   	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+ 	   	<title>CodeZoft - Solutions</title>
+ 	   	<script type="text/javascript">
+ 	   	</script>
+	</head>
+	<body>
+		<div id="contenedor">
+			<header>
+				<nav>
+				<ul>
+					<li><a href="#">Inicio </a></li>
+					<li><a href="">Promociones </a></li>
+					<li><a href="">Hosting </a></li>
+					<li><a href="">Blog </a></li>
+					<li><a href="">Descargas </a></li>
+					<li><a href="">Área de Clientes </a></li>
+				</ul>
+			</nav>
+				<div id="logo">
+
+				</div>
+			</header><!-- /header -->
+
+			<div id="menu_izquierdo">
+				<ul>
+					<li><a href="">SOPORTE TÉCNICO </a></li>
+					<li><a href="">DISEÑO DE PAGINAS WEB </a></li>
+					<li><a href="">WEB HOSTING </a></li>
+					<li><a href="">VENTA DE PROGRAMAS </a></li>
+					<li><a href="">VENTA DE VIDEOJUEGOS </a></li>
+					<li><a href="">ASESORÍA TÉCNICA </a></li>
+				</ul>
+			</div>
+
+			<div id="principal">
+
+			</div>
+		</div>
+	</body>
+</html>
